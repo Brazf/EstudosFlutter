4c399e5423df57bf91172eebeaa59d391ee7c631
Fábio Braz <fabiobrazferreira@gmail.com>
2022-10-29 18:49:02 -0300

    Primeira Tela

--- conteudo1/lib/main.dart
+++ conteudo1/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+main (){
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Text("Olá, Flutter!"),
+    );
+  }
+}
