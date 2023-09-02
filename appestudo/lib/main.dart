bdb3d5eb371b2657ee74fc674e792b1dc68638be
Fábio Braz <fabiobrazferreira@gmail.com>
2023-09-02 17:38:25 -0300

    Update main.dart

--- appestudo/lib/main.dart
+++ appestudo/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter com erro de conexão'),
+      home: const MyHomePage(title: 'App IFPE'),
     );
   }
 }
